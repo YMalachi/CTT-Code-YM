e2fc4b8044c9da00b57ab007497e5447fa8b87b3
YMalachi <yotamm10@gmail.com>
2025-01-06 17:17:46 +0200

    Skipping Auto-Exporting
    We decided to skip the automatic export function at the moment.
    Implemented some new core classes for video processing as well as other types of processing.

--- setup/CoreClasses.py
+++ setup/CoreClasses.py
@@ -1,21 +1,70 @@
-# Here I define core classes of the project like the GlobalContainer class.
-# It will be dynamically adjusted and passed whenever needed.
-# This is used to stimulate a g_pool instance which is used by many pupil labs' plugins
-# and core utilities.
+# Here I define core classes of the project, this serves as a base to most of the logic and tools.
 
-class GlobalContainer:
-    def __init__(self):
-        self.plugin_by_name = {}
-        self.plugins = None
-        self.rec_dir = None
-        self.user_dir = None
-        self.min_data_confidence = 0.6
-        self.timestamps = None
-        self.camera_render_size = None
-        self.gaze_positions = None
-        self.pupil_positions = None
-        self.fixations = None
-        self.topics = (
-            "notify.world_process.",
-                       )
+import os
+
+
+class DataContainer:
+    def __init__(self, data_path):
+        """
+        This class is the base for all data manipulations, it solely contains the folder with all test subjects data.
+        :param data_path:
+        """
+        if not isinstance(data_path, str):
+            raise TypeError("data_path must be a string.")
+        self.data_path = data_path
+
+    def __repr__(self):
+        return f"DataContainer(data_path={self.data_path})"
+
+class ProcessingContainer(DataContainer):
+    def __init__(self, data_path, subject_name, pl_path = None, uni_path = None, default_flag = True):
+        """
+        This is the base class for all processing procedures. It contains all necessary information for processing
+        purposes. Each test subject should have one of these, otherwise processing won't work with this subject.
+
+        :param data_path: Path to the entire data of all the test subjects folder. This will be passed to create a DataContainer instance.
+        :param subject_name: The name of the subject as written in the folder (e.g. YM696).
+        :param default_flag: A bool with value True if you're using the default data structure as mentioned in the documentation
+        """
+        super().__init__(data_path=data_path)
+        self.data_path = data_path
+        self.subject_name = subject_name
+        self.default_flag = default_flag
+        rec_path_intermediate = os.path.join(self.data_path, subject_name)
+        self.rec_path = os.path.join(rec_path_intermediate, 'REC_ET')  # this is the first directory in each subject
+        if not os.path.isdir(self.rec_path): # check if it exists
+            raise FileNotFoundError(f"The required directory does not exist: {self.rec_path}")
+
+        if self.default_flag is True:
+            # Construct and check paths
+            try:
+                # PL paths
+                self.pl_path = os.path.join(self.rec_path, 'PL')
+                os.path.isdir(self.pl_path)
+                if not os.path.isdir(self.pl_path):
+                    raise FileNotFoundError(f"The required directory does not exist: {self.pl_path}")
+
+                # Unity paths
+                self.uni_path = os.path.join(self.rec_path, 'UNI')
+                if not os.path.isdir(self.uni_path):
+                    raise FileNotFoundError(f"The required directory does not exist: {self.uni_path}")
+
+            except FileNotFoundError as e:
+                raise e
+
+            except Exception as e:
+                # catch any other unexpected errors
+                raise RuntimeError(f"An unexpected error occurred: {e}")
+
+        # If default_flag is False then we require pl_path and uni_path as input
+        elif pl_path or uni_path is None:
+            raise TypeError(f"If your data doesn't have a default structure, please provide pl_path and uni_path as well")
+
+        else:
+            self.pl_path = pl_path
+            self.uni_path = uni_path
+
+
+
+
 
--- src/utils/preprocessing.py
+++ src/utils/preprocessing.py
@@ -1,18 +1,28 @@
 import os
 from pathlib import Path
 import numpy as np
+import pandas as pd
+import av
+import time
+import logging
 
-from video_export.plugins.world_video_exporter import World_Video_Exporter
-from video_capture import File_Source
+# Our own libraries
+from CoreClasses import DataContainer,ProcessingContainer
+
+# Initializing log
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s"
+)
 
 class Preprocessor:
-    def __init__(self, g_pool, output_dir=None):
+    def __init__(self, input_dir, output_dir=None):
         """
         Initializes the Preprocessor with a shared g_pool object and output directory.
         :param g_pool: The GlobalContainer object holding global paths and state.
         :param output_dir: The directory to store processed data. Defaults to 'data' inside the project's directory.
         """
-        self.g_pool = g_pool
+        self.input_dir = input_dir
         self.output_dir = output_dir or os.path.join(os.getcwd(), 'data')
 
     def get_subject_folders(self):
@@ -49,41 +59,134 @@ class Preprocessor:
 
         return output_structure
 
+def validate_path(path, error_message):
+    if not os.path.exists(path):
+        logging.error(error_message)
+        raise FileNotFoundError(error_message)
 
-    def export_vid_for_subject(self, plugin_initializers):
-        from shared_modules.plugin import Plugin_List
-
-        # Determine what would be the last frame of the vid:
-        # Ensure world_timestamps.npy exists
-        timestamps_path = os.path.join(self.g_pool.rec_dir, "world_timestamps.npy")
-        if not os.path.exists(timestamps_path):
-            raise FileNotFoundError(f"Timestamps file not found: {timestamps_path}")
-        timestamps = np.load(timestamps_path) # load timestamps
-
-        start_frame = 0
-        end_frame = len(timestamps) - 1
-        export_range = (start_frame,end_frame)
-        export_dir = self.output_dir
-
-        # Updates plugins dynamically
-        self.g_pool.plugins = Plugin_List(self.g_pool, plugin_initializers=plugin_initializers)
-
-        # Export video
-        exporter = World_Video_Exporter(self.g_pool)
-        exporter.export_data(export_range,export_dir)
-
-
-    def export_all(self):
+class VideoPreprocessor: # instead of inheriting ProcessingContainer im passing its attributes directly
+    def __init__(self, parent, trail):
         """
-        Exports videos for all subjects in g_pool.rec_dir.
+        This class holds and utilize data for video processing.
+        :param trail: Trail type (e.g. trail B)
         """
-        output_structure = self.create_output_structure()
-        for subject_id, subject_output_dir in output_structure.items():
-            subject_folder = os.path.join(self.g_pool.rec_dir, subject_id)
-            print(f"Starting export for subject: {subject_id}")
+        # Creating the paths
+        self.subject_name = parent.subject_name
+        self.data_path = parent.data_path
+        self.pl_path = parent.pl_path
+        self.uni_path = parent.uni_path
+        self.uni_trail_name = self.subject_name + '_' + trail + '.txt'
+        self.uni_trail_path = os.path.join(self.uni_path, self.uni_trail_name) # this is the path to unity's trail file
+        # Creating a synchronized dict of matching Unity files and PL directory (for each trail there is one of each)
+        uni_pl_sync_dict = self.match_pl_uni()
+        self.pl_trail_name = uni_pl_sync_dict.get(self.uni_trail_name, None)
+        if not self.pl_trail_name:
+            logging.error(f"No matching PL directory found for {self.uni_trail_name} in Unity files {self.uni_path}.")
+            raise KeyError(f"No matching PL directory found for {self.uni_trail_name} in Unity files {self.uni_path}.")
+
+        # More paths
+        self.trail_path = os.path.join(parent.pl_path, self.pl_trail_name) #this is the path to the trail's directory, e.g. /005/
+        self.pl_exports_path = os.path.join(self.trail_path, 'exports') # path to exports directory inside each trail. this only exists of you've already exported via Pupil Labs' software
+        if not os.path.isdir(self.pl_exports_path): # checking if you've exported the vid...
+            raise FileNotFoundError(f"There is no exports directory in this path {self.trail_path}. Double check if you went through export process before.")
+
+        specific_export = max(os.listdir(self.pl_exports_path))
+
+        # Creating paths and checking if they exist
+        try:
+            self.specific_export_path = os.path.join(self.pl_exports_path, specific_export) # path to export 000 for example
+            validate_path(self.specific_export_path, f"The required directory does not exist: {self.specific_export_path}")
+
+            self.vid_path = os.path.join(self.specific_export_path, 'world.mp4') # path to the world video
+            validate_path(self.vid_path, f"Video not found in: {self.specific_export_path}")
+
+            self.pl_timestamps = os.path.join(self.specific_export_path, 'world_timestamps.csv') # path to the world timestamps
+            validate_path(self.pl_timestamps, f"Timestamps csv file not found in: {self.specific_export_path}")
+
+            self.pl_fixations = os.path.join(self.specific_export_path, 'fixations.csv') # path to Pupil Labs' fixations
+            validate_path(self.pl_fixations, f"Fixations csv file not found in: {self.specific_export_path}")
+
+            self.export_info = os.path.join(self.specific_export_path, 'export_info.csv') # path to Pupil Labs' export_info txt file
+            validate_path(self.export_info, f"Export_info csv file not found in: {self.specific_export_path}")
+
+        except FileNotFoundError as e:
+            raise e
+        except Exception as e:
+            raise e
+
+    def match_pl_uni(self):
+        """
+        Matches the names of folders and files of Pupil Labs and Unity so that
+        we know which PL folder matches which trial, using the "file modification timestamp" attribute.
+        :return: A dict consisting of: (key : value) = (unity_txt_file_name : corresponding PL folder)
+        """
+
+        # I first create the file name lists, then check if everything is as expected and only then match the files.
+        unity_file_check_list = [self.subject_name + "_Reference_Calibration_1.txt", self.subject_name + "_P1.txt", self.subject_name + "_P1A.txt", self.subject_name + "_T1.txt", self.subject_name + "_P2.txt", self.subject_name + "_P2A.txt", self.subject_name + "_T2.txt"] # these are the expected names
+        unity_file_list = os.listdir(self.uni_path)
+        if len(unity_file_list) >= len(unity_file_check_list):
+            logging.info(f"There are more files in {self.uni_path} than expected.")
+        for file in unity_file_check_list[:]:  # iterate over a copied list
+            if file not in unity_file_list:
+                unity_file_check_list.remove(file)  # modify the original list
+                logging.error(f"There is a missing file or a file with unexpected name in {self.uni_path}, file name: {file}")
+
+        # Checking if everything is as expected!
+        # First checking for missing files
+        for file in unity_file_check_list: # checking in unity folder
             try:
-                self.export_vid_for_subject(subject_folder, subject_output_dir)
-            except Exception as e:
-                print(f"An error occurred while exporting for subject {subject_id}: {e}")
+                if file not in unity_file_list: # check for missing files
+                    unity_file_check_list.remove(file)
+                    raise UserWarning(f"There is a missing file or a file with unexpected name in {self.uni_path}, file name: {file}")
+            except UserWarning as w:
+                logging.warning(f"There is a missing file or a file with unexpected name in {self.uni_path}, file name: {file}")
+                logging.info(f"{file} have been removed from the Unity files list")
+
+        # Now checking if there are enough video dirs in PL's data
+        pl_dir_list = os.listdir(self.pl_path)
+        try:
+            if len(unity_file_list) > len(pl_dir_list):
+                raise Warning(f"There are not enough video directories in {self.pl_path}. Check for mismatches.")
             else:
-                print(f"Successfully exported data for subject: {subject_id}")
+                pass
+        except Warning:
+            logging.warning(f"There are not enough video directories in {self.pl_path}. Check for mismatches.")
+
+        # Extracting the creation timestamp
+        uni_file_dict = {} # a dictionary to save file modification tampstamps (key:value)=(file_name:timestamp)
+        for file in unity_file_check_list:
+            file_path = os.path.join(self.uni_path, file)
+            uni_file_dict[file] = os.path.getmtime(file_path)
+
+
+        pl_dir_dict = {} # same but (dir_name:timestamps)
+        for _dir in pl_dir_list:
+            pl_file_path = os.path.join(self.pl_path, _dir)
+            ts_path = os.path.join(pl_file_path, 'world_timestamps.npy') # using the timestamps is ideal because it is created in the same exact moment as the UNI files
+            pl_dir_dict[_dir] = os.path.getmtime(ts_path)
+
+        # Now creating a dict where (key:value) = (UNI file name:matching PL dir)
+        # I also make sure the dictionaries are sorted by keys (this condition should be satisfied already, but it's kept for better readability
+        uni_file_dict_list = sorted(uni_file_dict.items(), key= lambda item: item[1])
+        pl_dir_dict_list = sorted(pl_dir_dict.items(), key= lambda item: item[1])
+
+        # Creating the synchronized dict:
+        sync_dict = {}
+        try:
+            if len(pl_dir_dict_list) == len(uni_file_dict_list):
+                for i in range(len(uni_file_dict_list)):
+                    sync_dict[uni_file_dict_list[i][0]] = pl_dir_dict_list[i][0]
+            else:
+                raise ValueError(f"Mismatch between Unity files ({len(uni_file_dict_list)}) and PL directories ({len(pl_dir_dict_list)}).")
+        except ValueError:
+            logging.error(f"Mismatch between Unity files ({len(uni_file_dict_list)}) and PL directories ({len(pl_dir_dict_list)}).")
+        except Exception as e:
+            logging.error(f"An error occurred: {e}")
+        return sync_dict
+
+# #test
+# data = DataContainer(data_path=r'C:\Users\yotam\Desktop\Studies\year 3\CTT Project\CTT Yotam Malachi\YotamMalachi\data')
+# subject_name = 'AN755'
+# processing = ProcessingContainer(data_path=data.data_path, subject_name=subject_name)
+# vid_pro = VideoPreprocessor(processing, trail='T2')
+# vid_pro.match_pl_uni()
